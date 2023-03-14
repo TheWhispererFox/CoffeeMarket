2069f0559e461ed3f759589f45253d67218c4bf3
TheWhisperer <houstonplanes@gmail.com>
2023-03-14 19:10:40 +0300

    [Add] Orders Controller barebones

---
+++ OrderService/Data/OrderServiceContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OrderService.Models;
+
+namespace OrderService.Data
+{
+    public class OrderServiceContext : DbContext
+    {
+        public OrderServiceContext (DbContextOptions<OrderServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<OrderService.Models.Order> Order { get; set; } = default!;
+    }
+}
--- OrderService/Models/Order.cs
+++ OrderService/Models/Order.cs
@@ -2,5 +2,6 @@
 {
     public class Order
     {
+        public int Id { get; set; }
     }
 }
